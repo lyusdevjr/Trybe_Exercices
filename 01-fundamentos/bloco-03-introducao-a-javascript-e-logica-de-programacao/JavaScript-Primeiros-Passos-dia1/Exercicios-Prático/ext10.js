e1e2130e4ea55223af0488aeaa62c3d054b2472c
Luis Henrique <lyusdevjr@gmail.com>
2023-05-02 19:40:35 -0300

    Exercicios Concluido

--- 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/Exercicios-Prático/ext10.js
+++ 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/Exercicios-Prático/ext10.js
@@ -1,37 +1,11 @@
-let inss;
-let ir;
+let custo=(3000.2) + 300;
+let venda=400;
+let unidades=1000;
 
-let salarioBruto = 2500
-
-if (salarioBruto <= 1556.94) {
-  inss = salarioBruto * 0.08
-
-} else if (salarioBruto <= 2594.92) {
-  inss = salarioBruto * 0.09
-
-} else if (salarioBruto <= 5189.92) {
-  inss = salarioBruto * 0.11;
-
-} else {
-  inss = 570.88
+if(custo <= 0 || venda <= 0){
+  console.log("Não é possivel calcular o lucro se o valor de custo ou venda for 0")
 }
-
-let basedoSalario = salarioBruto - inss;
-
-
-if (basedoSalario <= 1903.98) {
-  ir = 0;
-} else if (basedoSalario <= 2826.65) {
-  ir = (basedoSalario * 0.075) - 142.80;
-
-} else if (basedoSalario <= 3751.05) {
-  ir = (basedoSalario * 0.15) - 354.80;
-
-} else if (basedoSalario <= 4664.68) {
-  ir = (basedoSalario * 0.225) - 636.13;
-
-} else {
-  ir = (basedoSalario * 0.275) - 869.36;
-};
-
-console.log("Salário: " + (basedoSalario - ir));
+else{
+  let lucro = (vendaunidades) - (custo*unidades);
+  console.log("O lucro foi de :" + lucro);
+}
