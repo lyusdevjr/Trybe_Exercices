353b87b940965612e10b453abffc0248d5e171ab
Luis Henrique <lyusdevjr@gmail.com>
2023-05-02 10:20:13 -0300

    Var e Primitivos

---
+++ 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/tiposPrimitivos/OperaçãoAritmetica.js
@@ -0,0 +1,16 @@
+// +, -, *, /, **(elevado)
+
+let salary = 3500;
+
+
+console.log("soma de Salario ",salary + salary);
+console.log("Subtraçãod e Salario", salary - salary);
+console.log("Multiplicação de Salario", salary * salary);
+console.log("Divisão do Salario", salary / salary);
+console.log("Elevado" ,3 ** 3);
+
+// incrementar ++ acrescenta 1 ao number
+// decrementa -- retira 1 ao number
+
+
+console.log(salary)
---
+++ 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/tiposPrimitivos/typesPrimitivos.js
@@ -0,0 +1,5 @@
+let movie = "Avengers"; // string literal , sequencias de caracteres que pode conter números e caracteres especiais.
+let score = 10; // number literal , mesmo que seja número quebrado ou inteiro será reconhecido pelo javascript.
+let isValid = true // boolean "verdadeiro" ou "falso".
+let name; // undefined , variavel que não recebeu nenhum valor.
+let color = null //  redefinição , variavel que começa com valor nulo esperando uma redefinição.
