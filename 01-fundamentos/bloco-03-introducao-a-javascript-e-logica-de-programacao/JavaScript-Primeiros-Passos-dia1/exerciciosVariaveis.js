6a21bf123edbdbc829bc301dd255235d7455b840
Luis Henrique <lyusdevjr@gmail.com>
2023-05-02 09:57:25 -0300

    Exercicios variaveis e exemplo variaveis

---
+++ 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/exerciciosVariaveis.js
@@ -0,0 +1,6 @@
+let myName = "Luis Henrique";
+let birthCity = "Salvador-BA"
+let birthYear = "nasci em 1996"
+
+
+console.log(myName, birthCity, birthYear)
---
+++ 01-fundamentos/bloco-03-introducao-a-javascript-e-logica-de-programacao/JavaScript-Primeiros-Passos-dia1/scripts.js
@@ -0,0 +1,13 @@
+let age = 20; /* Variavel do tipo let conseguimos reatribuir os valores */
+age = 30;
+let fullName = "Luis Henrique";
+
+const idade = 20; /* Variavel do tipo const não conseguimos reatribuir os valores */
+
+/* camelCase =  primeira palavra começa com minuscula e a segunda palavra começa com maiusculo */
+
+
+
+console.log(age);
+console.log(fullName);
+console.log(idade);
